c946fc5d558d7119cd2b291dfb76a4a5b6bf4713
Felipe Rezende <feliperezende.srz@gmail.com>
2020-07-03 18:20:03 -0300

    Convertendo o valor de entrada para json

--- car/car.ino
+++ car/car.ino
@@ -1,10 +1,13 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#include <Arduino_JSON.h>
 
 const char* SSID = "eu"; // rede wifi
 const char* PASSWORD = "eueueueu"; // senha da rede wifi
 
 String BASE_URL = "http://192.168.43.235:8080/command";
+
+JSONVar command; // Fields: ['velocity', 'direction']
 
 void initSerial();
 void initWiFi();
@@ -22,6 +25,16 @@ void loop() {
 
   String response = getCommand();
 
-  Serial.println("##[RESULT]## ==> " + response);
-  
+//  Serial.println("##[RESULT]## ==> " + response);
+
+  if (response != "") {
+     command = JSON.parse(response);
+
+//     Serial.print("{ velocity: " + ((int) command["velocity"]) + ", direction" + ((int) command["direction"]) );
+     Serial.print("{ velocity: ");
+     Serial.print((int) command["velocity"]);
+     Serial.print(", direction: ");
+     Serial.print((int) command["direction"]);
+     Serial.println(" }");
+  }
 }
